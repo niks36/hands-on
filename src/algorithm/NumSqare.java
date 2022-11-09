15721c57b23a675506398bf19e4b1de576eff003
Niket Shah <shahniket.36@gmail.com>
2022-11-09 11:50:28 +0530

    Create NumSqare.java

---
+++ src/algorithm/NumSqare.java
@@ -0,0 +1,23 @@
+public class NumSquare {
+
+  public int numSquares(int n){
+    int arr[]=new int[n+1];
+    return helper(n,arr);
+  }
+
+  public int helper(int n,int arr[]){
+    if(n<0) return Integer.MAX_VALUE;  //Taking min later on
+    if(n==0) return 0;
+
+    if(arr[n]>0) return arr[n];
+
+    int min=n;
+
+    for(int i=1;i*i<=n;i++){
+      min=Math.min(min,helper(n-(i*i),arr));
+    }
+    arr[n]=min+1;
+    return min+1;
+  }
+
+}
